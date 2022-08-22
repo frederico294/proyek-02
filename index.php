6709bbf59ef298e6a57be9e6c557bc8320a36b27
BoomSlang <boom.slang.r7@gmail.com>
2022-08-22 20:08:35 +0700

    edit php

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
-<?php 
-include('connection.php'); 
+<?php
+include('connection.php');
 session_start();
 error_reporting(0);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="style.css" rel="stylesheet" type="text/css" />
-    <style type = "text/css">
+    <style type="text/css">
         * {
             font-family: "Trebuchet MS";
         }
-        
+
         h1 {
             text-transform: uppercase;
             color: #34e8eb;
@@ -76,41 +77,42 @@ error_reporting(0);
             background-color: black;
         }
 
-        img{
+        img {
             float: right;
         }
     </style>
 </head>
+
 <body>
     <h1>Sportifun</h1>
     <h2>Data Product</h2>
-    <a href = "add_product.php">+ &nbsp; Add product</a>
+    <a href="add_product.php">+ &nbsp; Add product</a>
     <br>
-    <img src = "phoo2.jpg" alt = "Trulli" width = "500" height = "300">
+    <img src="phoo2.jpg" alt="Trulli" width="500" height="300">
     <table>
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Buy Price</th>
-                    <th>Sell Price</th>
-                    <th>Image</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $query = "SELECT * FROM product ORDER BY id ASC";
-                    $result = mysqli_query($conn, $query);
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Buy Price</th>
+                <th>Sell Price</th>
+                <th>Image</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $query = "SELECT * FROM product ORDER BY id ASC";
+            $result = mysqli_query($conn, $query);
 
-                    if(!$result) {
-                        die("Query error: " . mysqli_error($conn));
-                    }
-                    $no = 1;
+            if (!$result) {
+                die("Query error: " . mysqli_error($conn));
+            }
+            $no = 1;
 
-                    while($row = mysqli_fetch_assoc($result)) {
-                ?>
+            while ($row = mysqli_fetch_assoc($result)) {
+            ?>
 
                 <tr>
                     <td><?php echo $no; ?></td>
@@ -118,22 +120,24 @@ error_reporting(0);
                     <td><?php echo substr($row['description'], 0, 200); ?></td>
                     <td>Rp <?php echo number_format($row['buy_price'], 0, ',', '.'); ?></td>
                     <td>Rp <?php echo number_format($row['sell_price'], 0, ',', '.'); ?></td>
-                    <td><img style = "width: 120px;" src = "image/<?php echo $row['image_product']; ?>"></td>
+                    <td><img style="width: 120px;" src="image/<?php echo $row['image_product']; ?>"></td>
                     <td>
-                        <a href = "edit_product.php?id=<?php echo $row['id']; ?>">Edit</a><br><br><br>
-                        <a href = "delete_process.php?id=<?php echo $row['id']; ?>" onclick = "return confirm('Are you sure?')">Delete</a>
+                        <a href="edit_product.php?id=<?php echo $row['id']; ?>">Edit</a><br><br><br>
+                        <a href="delete_process.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure?')">Delete</a>
                     </td>
                 </tr>
 
-                <?php
-                   $no++;
-                }
-                ?>
-            </tbody>
-        </table>
+            <?php
+                $no++;
+            }
+            ?>
+        </tbody>
+    </table>
     <br><br>
-        <a href = "logout.php">Logout</a>
-        <h1><?php date_default_timezone_set("Asia/Jakarta"); echo date("l, d F Y h:i a"); ?></h1>
+    <a href="logout.php">Logout</a>
+    <h1><?php date_default_timezone_set("Asia/Jakarta");
+        echo date("l, d F Y h:i a"); ?></h1>
     <script src="script.js"></script>
 </body>
+
 </html>
