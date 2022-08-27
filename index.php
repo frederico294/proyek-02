25cbe27740a67cf47d713544b8b18c34dbefa009
Frederico <frederico2947@gmail.com>
2022-08-27 23:11:17 +0700

    edit php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ error_reporting(0);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sportifun</title>
+    <title>Sportifun </title>
     <link href="style.css" rel="stylesheet" type="text/css" />
     <style type="text/css">
         * {
